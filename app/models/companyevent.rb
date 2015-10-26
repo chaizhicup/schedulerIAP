1c6d25c7b9976d30a7873e739e4449d26318dbf3
Abhinandan Aryadipta <aryarockstar@tamu.edu>
2015-10-25 22:07:40 -0300

    Modified views, companyevent controller for company code restructuring

--- app/models/companyevent.rb
+++ app/models/companyevent.rb
@@ -1,4 +1,8 @@
 class Companyevent < ActiveRecord::Base
     belongs_to :company
     belongs_to :event
+
+    accepts_nested_attributes_for :company
+    accepts_nested_attributes_for :event
+
 end
