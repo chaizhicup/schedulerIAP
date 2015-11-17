57fa7d281c20be2e03c6d25fdba49f4afaa5c279
Avinash Komanduri <komanduri.avinash@gmail.com>
2015-11-17 14:44:49 -0600

    Multi select for student level, appropriate changes in views

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,8 +5,9 @@ class Company < ActiveRecord::Base
   accepts_nested_attributes_for :companyevents
   accepts_nested_attributes_for :events
 
+  serialize :student_level,Array
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :name,:contact_person,:sponsor_level,:citizenship,:job_type,:student_level,:rep_1, presence: true
+	validates :name,:contact_person,:sponsor_level,:citizenship,:job_type,:rep_1, presence: true
   	validates :contact_email, presence: true, length: { maximum: 255 },
        		          format: { with: VALID_EMAIL_REGEX },
          	          uniqueness: { case_sensitive: false }
